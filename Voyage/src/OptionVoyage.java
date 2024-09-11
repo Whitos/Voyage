243f06d8e72839880b9e49f449c5ab0198957207
Whitos <pavel.tsanev@lycee-faure.fr>
2024-09-11 11:41:16 +0200

    done OptionVoyage

---
+++ Voyage/src/OptionVoyage.java
@@ -0,0 +1,28 @@
+public class OptionVoyage {
+    private String nom;
+    private double prix;
+    private boolean tarif_réduit;
+
+    final static double COEF_TARIF_REDUIT = 0.8;
+
+    public OptionVoyage(String nom, double prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public double getPrix() {
+        return this.prix;
+    }
+
+    @Override
+    public String toString() {
+        return  nom + "-> " + prix + " euros";
+    }
+
+    
+    
+}
